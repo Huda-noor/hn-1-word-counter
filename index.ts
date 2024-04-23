d65e7857d0fa2f56357ecce7e89a8c0e7ee673c5
Huda Noor <160596770+Huda-noor@users.noreply.github.com>
2024-04-23 20:53:57 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+// // declare a constant "answers" and assign results of inquirer pompt functions
+  
+console.log(chalk.bgGreenBright("Welcome to Word Counter!"))
+do{
+const answers: {
+    Sentence: string
+}= await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence! Which you wish to count words"
+    }
+]);
+
+const words = answers.Sentence.trim().split(" ")
+
+// // // print the array of wordsto the console
+
+console.log(chalk.bgYellowBright(words));
+// // // print the word count of the senteces to the console
+
+console.log(chalk.bgRedBright(`Word count in your sentence is ${words.length} words`));
+}while(true);
